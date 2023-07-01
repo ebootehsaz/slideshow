84de7c2601d0c55970c2712f388857a45dce3663
Ethan Bootehsaz <ethandb2024@gmail.com>
2023-07-01 16:53:17 -0700

    added loops

--- script.js
+++ script.js
@@ -1,79 +1,88 @@
 window.addEventListener('DOMContentLoaded', function() {
-    const delay = 5_000; 
+    const delay = 3_000; 
     const transitionTime = 1000;
     const fileInput = document.getElementById('file-input');
     const selectedCount = document.getElementById('selected-count');
     const startSlideshowBtn = document.getElementById('start-slideshow');
     const shuffleToggleBtn = document.getElementById('shuffle-toggle');
+    const loopToggleBtn = document.getElementById('loop-toggle');
     const slideshowImage = document.getElementById('slideshow-image');
     const slideshowContainer = document.getElementById('slideshow-container');
-
     
     let photos = [];
     let currentPhotoIndex = 0;
     let isShuffled = false;
-    
+    let isLooped = true;
+    loopToggleBtn.classList.toggle('active', isLooped);
+
     fileInput.addEventListener('change', function(event) {
       const fileList = event.target.files;
       
-      // Convert the FileList to an array
       photos = Array.from(fileList);
 
-      // Update the selected photo count
       selectedCount.textContent = photos.length + ' photo(s) selected';
     });
     
-    startSlideshowBtn.addEventListener('click', function() {
+    startSlideshowBtn.addEventListener('click', async function() {
       if (photos.length === 0) {
         alert('Please select photos to start the slideshow.');
         return;
       }
       
-      // Shuffle the photos if the shuffle option is enabled
-      if (isShuffled) {
-        shuffleArray(photos);
-      }
-      
-      startSlideshowBtn.disabled = true;
+    //   startSlideshowBtn.disabled = true;
       fileInput.disabled = true;
 
-      slideshowContainer.style.width = '100vw';
-      slideshowContainer.style.height = '100vh';
+      slideshowContainer.style.width = '99vw';
+      slideshowContainer.style.height = '99vh';
 
-      startSlideshow();
+      let x = 1; 
+
+      do {
+        startSlideshowBtn.disabled = true;
+        console.log("(re)starting slideshow");
+        if (isShuffled) {
+            shuffleArray(photos);
+        }
+        do {
+            x = await startSlideshow(); 
+        } while (x === 0);
+        startSlideshowBtn.disabled = false;
+      } while (isLooped);
+
     });
     
     function startSlideshow() {
-        if (currentPhotoIndex >= photos.length) {
-          // Slideshow has ended
-          startSlideshowBtn.disabled = false;
-          fileInput.disabled = false;
-          currentPhotoIndex = 0;
-          return;
-        }
-      
-        const photo = photos[currentPhotoIndex];
-        const reader = new FileReader();
-      
-        reader.addEventListener('load', function() {
-          slideshowImage.style.opacity = '0'; // Start with opacity 0
-      
-          // Wait for transition to complete
-          setTimeout(function() {
-            slideshowImage.src = reader.result;
-            slideshowImage.style.opacity = '1'; // Fade in the new photo
-          }, transitionTime);
-      
-          // Move to the next photo after 3 seconds
-          setTimeout(function() {
-            currentPhotoIndex++;
-            startSlideshow();
-          }, delay);
+        return new Promise((resolve) => {
+          if (currentPhotoIndex >= photos.length) {
+            // Slideshow has ended
+            currentPhotoIndex = 0;
+            resolve(1); 
+            return;
+          }
+        
+          const photo = photos[currentPhotoIndex];
+          const reader = new FileReader();
+        
+          reader.addEventListener('load', function() {
+            slideshowImage.style.opacity = '0'; 
+        
+            // Wait for transition to complete
+            setTimeout(function() {
+              slideshowImage.src = reader.result;
+              slideshowImage.style.opacity = '1'; // Fade in the new photo
+            //   selectedCount.textContent = slideshowImage.src;
+            }, transitionTime);
+        
+            // Move to the next photo after 3 seconds
+            setTimeout(function() {
+              currentPhotoIndex++;
+              resolve(0); 
+            }, delay);
+          });
+        
+          reader.readAsDataURL(photo);
         });
-      
-        reader.readAsDataURL(photo);
-    }
-      
+      }
     
     function shuffleArray(array) {
       for (let i = array.length - 1; i > 0; i--) {
@@ -84,8 +93,14 @@ window.addEventListener('DOMContentLoaded', function() {
 
     shuffleToggleBtn.addEventListener('click', function() {
         isShuffled = !isShuffled;
+        console.log(isShuffled);
         shuffleToggleBtn.classList.toggle('active', isShuffled);
-      });
+    });
+
+    loopToggleBtn.addEventListener('click', function() {
+        isLooped = !isLooped;
+        loopToggleBtn.classList.toggle('active', isLooped);
+    });
 
       
   });
